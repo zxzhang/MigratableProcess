d241401f4dd02311515948e1105914c3aa0d2f31
zhengxiz <zxzhang1991@gmail.com>
2014-09-11 18:28:49 -0400

    done...

--- clean.sh
+++ clean.sh
@@ -1,1 +1,2 @@
-rm ./src/edu/cmu/courses/ds/launchingProcesses/*.class ./src/edu/cmu/courses/ds/mprocess/*.class ./src/edu/cmu/courses/ds/transactionalFileStream/*.class
+rm -rf ./bin
+rm ./testCase/*.out
--- runSlave.sh
+++ runSlave.sh
@@ -1,7 +1,10 @@
 MasterHost=ghc23.ghc.andrew.cmu.edu
 
+rm -rf ./bin
+mkdir ./bin
+
 echo $MasterHost
 
-javac -classpath ./src:./jar/reflections-0.9.9-RC1-uberjar.jar:./jar/com.google.common_1.0.0.201004262004.jar:./jar/javassist-3.8.0.GA.jar ./src/edu/cmu/courses/ds/launchingProcesses/*.java ./src/edu/cmu/courses/ds/mprocess/*.java ./src/edu/cmu/courses/ds/transactionalFileStream/*.java
+javac -classpath ./jar/reflections-0.9.9-RC1-uberjar.jar:./jar/com.google.common_1.0.0.201004262004.jar:./jar/javassist-3.8.0.GA.jar -d ./bin ./src/edu/cmu/courses/ds/launchingProcesses/*.java ./src/edu/cmu/courses/ds/mprocess/*.java ./src/edu/cmu/courses/ds/transactionalFileStream/*.java
 
-java -classpath ./src:./jar/reflections-0.9.9-RC1-uberjar.jar:./jar/com.google.common_1.0.0.201004262004.jar:./jar/javassist-3.8.0.GA.jar edu.cmu.courses.ds.launchingProcesses.ProcessManager slave $MasterHost
+java -classpath ./bin:./jar/reflections-0.9.9-RC1-uberjar.jar:./jar/com.google.common_1.0.0.201004262004.jar:./jar/javassist-3.8.0.GA.jar edu.cmu.courses.ds.launchingProcesses.ProcessManager slave $MasterHost
