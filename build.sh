f7e69641ac94660272d280def1b78da7838e6dbe
zhengxiz <zxzhang1991@gmail.com>
2014-10-10 01:39:01 -0400

    FINAL

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -rf ./bin
 mkdir ./bin
-javac -classpath ./jar/reflections-0.9.9-RC1-uberjar.jar:./jar/com.google.common_1.0.0.201004262004.jar:./jar/javassist-3.8.0.GA.jar -d ./bin ./src/edu/cmu/courses/ds/launchingProcesses/*.java ./src/edu/cmu/courses/ds/mprocess/*.java ./src/edu/cmu/courses/ds/transactionalFileStream/*.java
+javac -classpath ./lib/reflections-0.9.9-RC1-uberjar.jar:./lib/com.google.common_1.0.0.201004262004.jar:./lib/javassist-3.8.0.GA.jar -d ./bin ./src/edu/cmu/courses/ds/launchingProcesses/*.java ./src/edu/cmu/courses/ds/mprocess/*.java ./src/edu/cmu/courses/ds/transactionalFileStream/*.java
